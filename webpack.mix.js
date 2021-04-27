0f2b69529fa209316b4b0476e876aca72cda6273
apolopena <penavilla@gmail.com>
2021-04-27 01:51:46 +0000

    ➕ ADD: support hot reload using browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,23 @@ const mix = require('laravel-mix');
 mix.ts('resources/js/index.tsx', 'public/js/index.js')
     .react()
     .sass('resources/sass/app.scss', 'public/css');
+
+const { execSync } = require('child_process');
+const PROXY_PORT=8001
+const BS_PORT=3005
+const GP_URL = execSync(`gp url ${BS_PORT}`)
+
+mix.browserSync({
+  proxy: {
+    target: `http://localhost:${PROXY_PORT}`, /*
+    proxyReq: [
+      function(proxyReq) {
+          proxyReq.setHeader('X-Forwarded-Host', `localhost:${GP_URL}`);
+      }
+    ],*/
+  },
+  port: BS_PORT,
+  socket: {
+    domain: GP_URL
+  }
+});
